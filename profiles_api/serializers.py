6039090a2ec04fc635ea4987dd76e09203b32e0a
Nastaran Yousefi <nastaranxyousefi@gmail.com>
2023-05-17 14:03:42 +0330

    added UserProfileSerializer

--- profiles_api/serializers.py
+++ profiles_api/serializers.py
@@ -1,6 +1,35 @@
 from rest_framework import serializers
 
+from profiles_api import models
+
 
 class NameSerializer(serializers.Serializer):
     """serializes name field for our ApiView"""
     name = serializers.CharField(max_length=50)
+
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    """serializes a user profile object"""
+    class Meta:
+        model = models.UserProfile
+        fields = ('id', 'name', 'email', 'password')
+
+        extra_kwargs = {
+            'password' : {
+                'write_only' : True,
+                'style' : {
+                    'input_type' :'password',
+                },
+            }
+        }
+
+
+    def create(self, validated_data):
+        """creates and returns a new user"""
+        user = models.UserProfile.objects.create_user(
+            name = validated_data['name'],
+            email = validated_data['email'],
+            password = validated_data['password'],
+        )
+
+        return user
