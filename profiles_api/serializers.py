260783c05a75c6bdedb5d91cd707d8031f23c16b
Nastaran Yousefi <nastaranxyousefi@gmail.com>
2023-05-19 15:49:28 +0330

    added ProfileFeedItemSerializer

--- profiles_api/serializers.py
+++ profiles_api/serializers.py
@@ -33,4 +33,16 @@ class UserProfileSerializer(serializers.ModelSerializer):
         )
 
         return user
-    
+
+
+class ProfileFeedItemSerializer(serializers.ModelSerializer):
+    """serializes profile feed items"""
+    class Meta:
+        model = models.ProfileFeedItem
+        fields = ('id', 'user_profile', 'status_text', 'created_on', 'updated_on')
+
+        extra_kwargs = {
+            'user_profile' : {
+                'read_only' : True,
+            }
+        }
